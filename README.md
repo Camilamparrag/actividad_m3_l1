9c62f7f70b041363078ec576f0ac08a2ece4919a
Camilamparrag <camilaparragon@gmail.com>
2026-03-09 01:17:02 +0000

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# actividad_m3_l1
-actividad_m3_l1
+Actividad_m3_l1
+
+🧠 Primer contacto con Python
+
+En este primer contacto con Python, lo más fácil fue entender cómo usar print() para mostrar información en pantalla y declarar variables para guardar datos. La sintaxis del lenguaje es bastante clara y fácil de leer, lo que facilita el aprendizaje cuando uno recién comienza a programar. 💻
+
+Por otro lado, lo más desafiante fue acostumbrarme a la forma correcta de escribir el código y evitar pequeños errores de sintaxis, como usar signos incorrectos o escribir mal alguna instrucción. ⚠️
+También resultó un poco complejo comprender cómo funcionan algunos módulos, como math, y cómo utilizar sus funciones dentro del programa.
+
+Sin embargo, con práctica y revisando los errores que aparecen, poco a poco se vuelve más fácil comprender cómo funciona Python. 🚀
+Esto hace que el proceso de aprendizaje sea progresivo y cada vez más interesante.
--- actividad_m3_l1/primeros_pasos.py
+++ actividad_m3_l1/primeros_pasos.py
@@ -1,22 +1,27 @@
-
-
 #IMPRIMIR MENSAJE PRINCIPAL
 
 print("¡Hola! Este es mi primer programa en Python.")
 
-nombre = "Camila Monserrat Parra González"
+
+#DECLARA VARIABLES SIMPLES
+
+#definir variables
+nombre = "Camila Monserrat Parra González" 
 edad = 33
 ciudad = "Arica"
 
+#aqui se muestra el resultado
 print("Nombre:", nombre)
 print("Edad:", edad)
 print("Ciudad:", ciudad)
+
 
 #REALIZAR OPERACIONES BASICAS
 
 a , b = 2 , 3 #definir variables
 resultado = a + b #se realizo la operacion de suma
-print("la suma es:", resultado) #aqui se muestra el resultado
+print("la suma es:", resultado) #aqui se muestra el resultado en pantalla 
+
 
 #EXPLORAR UN MODULO INTEGRADO
 
@@ -24,4 +29,4 @@ import math # importe el modulo math
 
 numero = 50 # guarde el número al que se calculara la raíz cuadrada
 resultado = math.sqrt(numero) # se uso la función sqrt() del módulo math para calcular la raíz cuadrada
-print("La raíz cuadrada es:", resultado) #aqui se muestra resultada en pantalla
+print("La raíz cuadrada es:", resultado) #aqui se muestra el resultado en pantalla
