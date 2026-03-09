9864098c4e9cb15611530d660a28df1c677237f5
Camilamparrag <camilaparragon@gmail.com>
2026-03-09 01:13:36 +0000

    finalizado 1,2,3,4

---
+++ actividad_m3_l1/primeros_pasos.py
@@ -0,0 +1,27 @@
+
+
+#IMPRIMIR MENSAJE PRINCIPAL
+
+print("¡Hola! Este es mi primer programa en Python.")
+
+nombre = "Camila Monserrat Parra González"
+edad = 33
+ciudad = "Arica"
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Ciudad:", ciudad)
+
+#REALIZAR OPERACIONES BASICAS
+
+a , b = 2 , 3 #definir variables
+resultado = a + b #se realizo la operacion de suma
+print("la suma es:", resultado) #aqui se muestra el resultado
+
+#EXPLORAR UN MODULO INTEGRADO
+
+import math # importe el modulo math 
+
+numero = 50 # guarde el número al que se calculara la raíz cuadrada
+resultado = math.sqrt(numero) # se uso la función sqrt() del módulo math para calcular la raíz cuadrada
+print("La raíz cuadrada es:", resultado) #aqui se muestra resultada en pantalla
